cfd71321ab42733240e64d6a9552811ae53254b9
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2023-02-27 19:09:16 -0300

    First Commit
    Add all files

---
+++ main.js
@@ -0,0 +1,7 @@
+const btnProj = document.querySelector('#button-proj');
+const btnIg = document.querySelector('#button-ig');
+const btnYt = document.querySelector('#button-yt');
+
+function abrirProj() {
+    alert('Alo');
+}
